72f252a35fcd6528cf3c5c7ee9a569523169b16c
hucarrot <hucarrot021@gmail.com>
2017-06-30 13:43:44 +0800

    错误修复

--- src/ios/Sms.m
+++ src/ios/Sms.m
@@ -9,32 +9,32 @@
     CDVPluginResult* pluginResult = nil;
     NSString* phoneNumber = [command.arguments objectAtIndex:0];
     NSString* textMessage = [command.arguments objectAtIndex:1];
-    
-    self.callbackID = command.callbackId;
-    
-    if (![MFMessageComposeViewController canSendText]) {
+
+      self.callbackID = command.callbackId;
+
+      if (![MFMessageComposeViewController canSendText]) {
         NSMutableDictionary* returnInfo = [NSMutableDictionary dictionaryWithCapacity:2];
-        
+
         [returnInfo setObject:@"SMS_FEATURE_NOT_SUPPORTED" forKey:@"code"];
         [returnInfo setObject:@"SMS feature is not supported on this device" forKey:@"message"];
-        
+
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsDictionary:returnInfo];
-        
+
         [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-        
+
         return;
     }
-    
+
     MFMessageComposeViewController *composeViewController = [[MFMessageComposeViewController alloc] init];
     composeViewController.messageComposeDelegate = self;
-    
+
     NSMutableArray *recipients = [[NSMutableArray alloc] init];
-    
+
     [recipients addObject:phoneNumber];
-    
+
     [composeViewController setBody:textMessage];
     [composeViewController setRecipients:recipients];
-    
+
     [self.viewController presentViewController:composeViewController animated:YES completion:nil];
 }
 
@@ -43,39 +43,43 @@
     BOOL succeeded = NO;
     NSString* errorCode = @"";
     NSString* message = @"";
-    
-    switch(result) {
-        case MessageComposeResultSent:
-            succeeded = YES;
-            message = @"Message sent";
-            break;
-        case MessageComposeResultCancelled:
-            message = @"Message cancelled";
-            errorCode = @"SMS_MESSAGE_CANCELLED";
-            break;
-        case MessageComposeResultFailed:
-            message = @"Message Compose Result failed";
-            errorCode = @"SMS_MESSAGE_COMPOSE_FAILED";
-            break;
-        default:
-            message = @"Sms General error";
-            errorCode = @"SMS_GENERAL_ERROR";
-            break;
-    }
-    
+
+      switch(result) {
+          case MessageComposeResultSent:
+                succeeded = YES;
+              message = @"Message sent";
+              break;
+          case MessageComposeResultCancelled:
+                message = @"Message cancelled";
+              errorCode = @"SMS_MESSAGE_CANCELLED";
+              break;
+          case MessageComposeResultFailed:
+                message = @"Message Compose Result failed";
+              errorCode = @"SMS_MESSAGE_COMPOSE_FAILED";
+              break;
+          default:
+                message = @"Sms General error";
+              errorCode = @"SMS_GENERAL_ERROR";
+              break;
+      }
+
     [self.viewController dismissViewControllerAnimated:YES completion:nil];
-    
+
     if (succeeded == YES) {
-        [super writeJavascript:[[CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:message]
-                                toSuccessCallbackString:self.callbackID]];
+        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:message];
+
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:self.callbackID];
+
     } else {
         NSMutableDictionary* returnInfo = [NSMutableDictionary dictionaryWithCapacity:2];
-        
+
         [returnInfo setObject:errorCode forKey:@"code"];
         [returnInfo setObject:message forKey:@"message"];
-        
-        [super writeJavascript:[[CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsDictionary:returnInfo]
-                                toErrorCallbackString:self.callbackID]];
+
+        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsDictionary:returnInfo];
+
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:self.callbackID];
+
     }
 }
 
